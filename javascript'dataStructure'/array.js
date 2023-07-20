8066a19f066f19acd9cef2288c324b5c6c0209e5
Allan Oliveira <allanolme@gmail.com>
2023-07-19 23:38:23 -0300

    Create array.js

---
+++ javascript'dataStructure'/array.js
@@ -0,0 +1,9 @@
+const elementos = ["car", "second", "apple", "third"];
+
+elementos.splice(3)
+elementos.shift()
+elementos.unshift("first")
+elementos.push("fourth")
+elementos.reverse()
+
+console.log(elementos)
