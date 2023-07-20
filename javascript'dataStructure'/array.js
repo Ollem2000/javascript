56942ff5187e3f606f3f29f56fdbbe9c9714396f
Allan Oliveira <allanolme@gmail.com>
2023-07-20 11:52:14 -0300

    Create Matriz.js

--- javascript'dataStructure'/array.js
+++ javascript'dataStructure'/array.js
@@ -1,9 +1,16 @@
 const elementos = ["car", "second", "apple", "third"];
 
-elementos.splice(3)
+elementos.splice(2, 1)
 elementos.shift()
 elementos.unshift("first")
 elementos.push("fourth")
 elementos.reverse()
 
 console.log(elementos)
+
+for(let itens of elementos){
+	console.log(itens)
+}
+
+const first = elementos.find(itens => itens == "first")
+console.log(first)
---
+++ javascript'dataStructure'/matriz.js
@@ -0,0 +1,7 @@
+const students = {
+	0: ["Joseph", 23, "MG"],
+	1: ["Briana", 22, "SP"],
+	2: ["Priscila", 23, "PR"]
+};
+console.log(students);
+console.log(students[0][2]);
