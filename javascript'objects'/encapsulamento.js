bb844301c276b6b46a1d399cbde9bf4d47f3700f
Allan Oliveira <allanolme@gmail.com>
2023-07-21 10:59:38 -0300

    Create encapsulamento.js

---
+++ javascript'objects'/encapsulamento.js
@@ -0,0 +1,18 @@
+class Poligono{
+	constructor(altura, largura){
+		this.altura = altura;
+		this.largura = largura;
+	}
+	
+	get area(){
+		return this.#calcularArea()
+	}
+	
+	#calcularArea(){
+		return this.altura * this.largura
+	}
+}
+
+let poligono = new Poligono(50, 60);
+console.log(poligono)
+console.log(poligono.area)
