68d9ddf55d542ccd102a61cb1e814c6a6c108a84
Allan Oliveira <allanolme@gmail.com>
2023-08-03 19:58:14 -0300

    Create fetch.js

---
+++ javascript'promises'/fetch.js
@@ -0,0 +1,2 @@
+//fetch é toda uma estrutura de promessa
+fetch("https://api.github.com/users/maykbrito").then( response => response.json()).then( data => fetch(data.repos_url)).then( res => res.json()).then( d => console.log(d))
