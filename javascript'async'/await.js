224e0e6f586bf4e1770497124cc58108e3c82248
Allan Oliveira <allanolme@gmail.com>
2023-08-07 21:48:14 -0300

    Update await.js

--- javascript'async'/await.js
+++ javascript'async'/await.js
@@ -3,8 +3,17 @@ const promessa = new Promise( function(resolve, reject){
 })
 
 async function start(){
-	const result = await promessa
-	console.log(result)
+	try{
+		const result = await promessa
+		console.log(result);
+	}
+	catch(e){
+		console.log(e);
+	}
+	finally{
+		console.log("Loading");
+	}
+	
 }
 
 start()
