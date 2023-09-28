682a9b167f985124bd153cb342332f2002fd3444
Allan Oliveira <allanolme@gmail.com>
2023-09-28 10:53:25 -0300

    create element-prop_title

---
+++ class-create-element/src/element-prop_title.js
@@ -0,0 +1,24 @@
+class CardTitle extends HTMLElement{
+    constructor(){
+        super();
+
+        const shadow = this.attachShadow({
+            mode: "open"
+        });
+        
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = this.getAttribute("title");
+
+        const style = document.createElement("style");
+        style.textContent = `
+            h1{
+                color: red;
+            }
+        `;
+
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+    }
+}
+
+customElements.define("card-title", CardTitle);
