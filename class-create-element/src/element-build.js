93a17eaf967a3be895076c9783e8e540be8147bc
Allan Oliveira <allanolme@gmail.com>
2023-09-29 23:12:02 -0300

    Create element-build.js

---
+++ class-create-element/src/element-build.js
@@ -0,0 +1,13 @@
+class CardSample extends HTMLElement{
+    constructor(){
+        super();
+
+        const shadow = this.attachShadow({
+            mode: "open" //Define se essa classe haverá ou não modificação externa
+        });        
+        
+        build();
+    }
+}
+
+customElements.define("card-sample", CardSample);
