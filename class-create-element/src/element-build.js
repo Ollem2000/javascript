200f3e87f497a5563981b33f5087c219806ed054
Allan Oliveira <allanolme@gmail.com>
2023-10-03 17:38:14 -0300

    fixed elemente-build

--- class-create-element/src/element-build.js
+++ class-create-element/src/element-build.js
@@ -5,37 +5,37 @@ class CardBuild extends HTMLElement{
         const shadow = this.attachShadow({
             mode: "open" //Define se essa classe haverá ou não modificação externa
         });
+        shadow.appendChild(this.build());
+        //shadow.appendChild(this.style());
+    };
         
-        build(){
-            const componentRoot = createElement("div");
-            componentRoot.setAttribute("class", "card");
+    build(){
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
 
-                const cardLeft = document.createElement("div");
-                cardLeft.setAttribute("class", "card__left");
+            const cardLeft = document.createElement("div");
+            cardLeft.setAttribute("class", "card__left");
 
-                    const author = document.createElement("span");
-                    const linkTitle = document.createElement("a");
-                    const newsContent = document.createElement("p");
-                    cardLeft.appendChild(author);
-                    cardLeft.appendChild(linkTitle);
-                    cardLeft.appendChild(newsContent);
+                const author = document.createElement("span");
+                const linkTitle = document.createElement("a");
+                const newsContent = document.createElement("p");
+                cardLeft.appendChild(author);
+                cardLeft.appendChild(linkTitle);
+                cardLeft.appendChild(newsContent);
 
-                const cardRight = document.createElement("div");
-                cardRight.setAttribute("class", "card__right");
+            const cardRight = document.createElement("div");
+            cardRight.setAttribute("class", "card__right");
 
-                    const newsImage = document.createElement("img");
-                    cardRight.appendChild(newsImage);
+                const newsImage = document.createElement("img");
+                cardRight.appendChild(newsImage);
 
-            componentRoot.appendChild(cardLeft);
-            componentRoot.appendChild(cardRight);
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
 
-            return componentRoot;
-        };
-        
-        style(){
-
-        };
-    }
+        return componentRoot;
+    };
+    
+    //style(){};
 }
 
 customElements.define("card-build", CardBuild);
