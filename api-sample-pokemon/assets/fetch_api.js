1f73669ba9fbdc9ae8171735d8999f21d7d2be4b
Allan Oliveira <allanolme@gmail.com>
2023-10-05 18:02:45 -0300

    Update fetch_api.js

--- api-sample-pokemon/assets/fetch_api.js
+++ api-sample-pokemon/assets/fetch_api.js
@@ -1,31 +1,8 @@
-const api_limit = 10;
-const api_offset = 0;
-const api_link = `https://pokeapi.co/api/v2/pokemon?offset=${api_offset}&limit=${api_limit}`;
-
-function toHTML(pokemon){
-    return `
-    <article class="pokemon_card">
-        <div class="img">
-            <img alt="${pokemon.name}">
-        </div>
-        <div class="name">
-            <h3>${pokemon.name}</h3>
-        </div>
-    </article>
-    `
-};
-
-const pokemon_list = document.getElementById("pokemon-load");
-
-fetch(api_link)
+const pokeApi = {};
+pokeApi.getPokemons = (offset = 0, limit = 20) => {
+    const api_link = `https://pokeapi.co/api/v2/pokemon?offset=${api_offset}&limit=${api_limit}`;
+    return fetch(api_link)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .then((api_list) => {
-        for (let i = 0; i < api_list.length; i++){
-            const pokemon = api_list[i];
-            pokemon_list.innerHTML += toHTML(pokemon);
-        };
-    })
-    //.then((data) => console.log(data))
     .catch((error) => console.log(error))
-    .finally(() => console.log("Concluído!"));
+}
