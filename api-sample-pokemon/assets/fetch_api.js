67098d55a397658bd1f5c38b221507b9f8be4e8c
Allan Oliveira <allanolme@gmail.com>
2023-10-09 17:58:38 -0300

    Update fetch_api.js

--- api-sample-pokemon/assets/fetch_api.js
+++ api-sample-pokemon/assets/fetch_api.js
@@ -1,8 +1,15 @@
 const pokeApi = {};
+
+pokeApi.getPokemonsDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
 pokeApi.getPokemons = (api_offset = 0, api_limit = 20) => {
     const api_link = `https://pokeapi.co/api/v2/pokemon?offset=${api_offset}&limit=${api_limit}`;
     return fetch(api_link)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.log(error))
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetail))
+    .then((detail_requests) => Promise.all(detail_requests))
+    .then((pokemons_details) => pokemons_details)
 }
